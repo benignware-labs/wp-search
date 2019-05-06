9e20bd183c6cfc8e1d390fd22e71ef55a2a9b246
Rafael Nowrotek <mailbenignware.com>
2019-05-06 18:52:27 +0200

    Fix version update

--- search-xt.php
+++ search-xt.php
@@ -4,7 +4,7 @@
  Plugin Name: Search XT
  Plugin URI: http://github.com/benignware/wp-search-xt
  Description: Wordpress Search Extensions
- Version: 0.0.3
+ Version: 0.0.2
  Author: Rafael Nowrotek, Benignware
  Author URI: http://benignware.com
  License: MIT
